0baa1ddd5ecf98dd164df9f3db56db2c2233e31e
Tim <timothy.george.taylor@gmail.com>
2022-09-20 14:09:29 -0700

    STYLE: changed default image

--- src/lib/load-assets.js
+++ src/lib/load-assets.js
@@ -1,4 +1,4 @@
-import Photo from "../assets/pexels-alex-andrews-3805983.jpeg";
+import Photo from "../assets/Desolate_offworld_space_with_swirling_galaxies.png"
 import Sample from "../assets/TGT-disquiet0482.wav";
 
 export const loadPhoto = (image) => image ??  Photo;
