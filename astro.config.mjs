1004797d4199fc7c506a0b688050c70b5fb8c5aa
Tim <timothy.george.taylor@gmail.com>
2024-04-10 20:09:53 -0700

    add [global] vercel ssr integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
